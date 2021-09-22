3b7373324fb1b5b440ced5a0945787717d896389
alexercolin <alex_ercolin@hotmail.com>
2021-09-22 20:19:14 -0300

    fix: identation of the test

--- src/Order.test.ts
+++ src/Order.test.ts
@@ -4,12 +4,24 @@ test("Should calculate the area of circle", function () {
   const cpfNumber = "111.444.777-35";
   const purchaseDescription = "Teclado Gamer";
   const price = 150;
-  const quantity = 1
-  const discount = 5
+  const quantity = 1;
+  const discount = 5;
 
-  const order = new Order(cpfNumber,purchaseDescription,price,quantity,discount);
+  const order = new Order(
+    cpfNumber,
+    purchaseDescription,
+    price,
+    quantity,
+    discount
+  );
   expect(order.orderPrice()).toBe(142.5);
 
-  const cpftest = new Order(cpfNumber,purchaseDescription,price,quantity,discount);
-  expect(cpftest.cpfValidator).toBeTruthy()
+  const cpftest = new Order(
+    cpfNumber,
+    purchaseDescription,
+    price,
+    quantity,
+    discount
+  );
+  expect(cpftest.cpfValidator).toBeTruthy();
 });
