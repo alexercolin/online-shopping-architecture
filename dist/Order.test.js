4958ce90aaa33e2d9709d0a4b3e8cc61bd36ebb5
alexercolin <alex_ercolin@hotmail.com>
2021-09-24 13:26:29 -0300

    fix: test to validate cpf and order

--- dist/Order.test.js
+++ dist/Order.test.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Order_1 = __importDefault(require("./Order"));
-test("Should calculate the area of circle", function () {
+test("Should calculate the order amount", function () {
     const cpfNumber = "111.444.777-35";
     const purchaseDescription = "Teclado Gamer";
     const price = 150;
@@ -12,6 +12,13 @@ test("Should calculate the area of circle", function () {
     const discount = 5;
     const order = new Order_1.default(cpfNumber, purchaseDescription, price, quantity, discount);
     expect(order.orderPrice()).toBe(142.5);
+});
+test("Should validate if the cpf is valid", function () {
+    const cpfNumber = "111.444.777-35";
+    const purchaseDescription = "Teclado Gamer";
+    const price = 150;
+    const quantity = 1;
+    const discount = 5;
     const cpftest = new Order_1.default(cpfNumber, purchaseDescription, price, quantity, discount);
     expect(cpftest.cpfValidator).toBeTruthy();
 });
--- src/Order.test.ts
+++ src/Order.test.ts
@@ -1,6 +1,6 @@
 import Order from "./Order";
 
-test("Should calculate the area of circle", function () {
+test("Should calculate the order amount", function () {
   const cpfNumber = "111.444.777-35";
   const purchaseDescription = "Teclado Gamer";
   const price = 150;
@@ -15,6 +15,15 @@ test("Should calculate the area of circle", function () {
     discount
   );
   expect(order.orderPrice()).toBe(142.5);
+
+});
+
+test("Should validate if the cpf is valid", function () {
+  const cpfNumber = "111.444.777-35";
+  const purchaseDescription = "Teclado Gamer";
+  const price = 150;
+  const quantity = 1;
+  const discount = 5;
 
   const cpftest = new Order(
     cpfNumber,
